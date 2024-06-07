5931431bf5bfa9c6a17cb9a58a40689a884281ac
Florxncia <163814428+Florxncia@users.noreply.github.com>
2024-06-07 12:12:55 +1000

    Delete IAB207_Tekkit_A3 directory

--- IAB207_Tekkit_A3/README.md
+++
@@ -1,4 +0,0 @@
-# teamlonteez
-
-hi im fredella w stress bgt skrg just tryna figure out github
-hi
--- IAB207_Tekkit_A3/create_db.py
+++
@@ -1,7 +0,0 @@
-from Tekkitregistration import db, create_app
-app = create_app()
-ctx = app.app_context()
-ctx.push()
-db.drop_all()
-db.create_all()
-quit()
